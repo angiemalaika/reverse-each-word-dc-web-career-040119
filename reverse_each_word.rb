145285a1f0808b68f667f025b607f4192c138a2d
Angela Mumba <malaika.mumba@gmail.com>
2019-03-29 05:13:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,12 @@
 def reverse_each_word(sentence)
-  sentence = "Hello there, and how are you?" 
   reversed = sentence.split(" ").collect do |word|
   word.reverse
+
 end
   reversed.join(" ")
 end
  
 def second_method_reverse_each_word(sentence)
-
 sentence = "Hi again, just making sure it's reversed!"
   sentence.split.collect {|word| word.reverse}.join(" ")
 
